cd354c3ccbd9a6379b5da200e3e64f0bd246ffda
Valeria48562 <vblagodatskaja@gmail.com>
2024-01-30 12:36:20 +0200

    create Input component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,57 @@ import Card from "./components/Card";
 import Button from "./components/Button";
 import Input from "./components/Input";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [address, setAddress] = useState("");
+  const handleAddEducation = () => {
+    console.log("handleAddEducation");
+  };
+  console.log(fullName, email, phoneNumber, address);
   return (
     <div className="flex-col-gaps">
       <Card></Card>
-      <Button icon="+" color="dark">
+      <Button icon="+" color="dark" onClick={handleAddEducation}>
         Add education
       </Button>
       <Button icon="trash">Delete</Button>
       <Button>Cancel</Button>
       <Button color="dark">Save</Button>
-      <Button icon="+">Add experience</Button>
-      <Input></Input>
+      <Button icon="+" color="dark">
+        Add experience
+      </Button>
+      <Input
+        label="Full name:"
+        type="text"
+        placeholder="Valeria Blagodatskaja"
+        setValue={setFullName}
+        value={fullName}
+      />
+      <Input
+        label="Email:"
+        type="email"
+        placeholder="valeria@gmail.com"
+        setValue={setEmail}
+        value={email}
+      />
+      <Input
+        label={"Phone number:"}
+        type={"tel"}
+        placeholder={"+372..."}
+        setValue={setPhoneNumber}
+        value={phoneNumber}
+      />
+      <Input
+        label={"Address:"}
+        type={"text"}
+        placeholder={"Tallinn, Estonia"}
+        setValue={setAddress}
+        value={address}
+      />
     </div>
   );
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,9 +3,12 @@ import "./Button.css";
 import Plus from "../assets/plus.svg?react";
 import Trash from "../assets/trash.svg?react";
 
-export default function Button({ children, className, icon, color }) {
+export default function Button({ children, className, icon, color, onClick }) {
   return (
-    <button className={clsx("button", color === "dark" && "dark", className)}>
+    <button
+      onClick={onClick}
+      className={clsx("button", color === "dark" && "dark", className)}
+    >
       <div className="button-container">
         {icon === "+" && <Plus className="svg" />}
         {icon === "trash" && <Trash className="svg" />}
