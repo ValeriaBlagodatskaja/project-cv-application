8336d2844a191fa07894e6234a6b192b574e40a2
Valeria48562 <vblagodatskaja@gmail.com>
2024-01-30 08:46:10 +0200

    create button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import Card from "./components/Card";
+import Button from "./components/Button";
+import Input from "./components/Input";
 import "./App.css";
 
 function App() {
   return (
-    <>
-      <Card className="hhhh"></Card>
+    <div className="flex-col-gaps">
       <Card></Card>
-      <Card></Card>
-    </>
+      <Button icon="+" color="dark">
+        Add education
+      </Button>
+      <Button icon="trash">Delete</Button>
+      <Button>Cancel</Button>
+      <Button color="dark">Save</Button>
+      <Button icon="+">Add experience</Button>
+      <Input></Input>
+    </div>
   );
 }
 
---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+import "./Button.css";
+import Plus from "../assets/plus.svg?react";
+import Trash from "../assets/trash.svg?react";
+
+export default function Button({ children, className, icon, color }) {
+  return (
+    <button className={clsx("button", color === "dark" && "dark", className)}>
+      <div className="button-container">
+        {icon === "+" && <Plus className="svg" />}
+        {icon === "trash" && <Trash className="svg" />}
+        {children}
+      </div>
+    </button>
+  );
+}
