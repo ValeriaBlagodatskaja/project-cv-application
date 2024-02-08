3ff4856e4f3b097d3130df189925f1836f385ef2
Valeria48562 <vblagodatskaja@gmail.com>
2024-02-08 11:12:19 +0200

    create exampleCVdata component

---
+++ ExampleCVData.jsx
@@ -0,0 +1,46 @@
+const exampleCVData = {
+  personalInfo: {
+    fullName: "Example Name",
+    email: "example@email.com",
+    phoneNumber: "+123456789",
+    address: "Example Address",
+  },
+  educationInfo: [
+    {
+      school: "University",
+      degree: "bakalaurus",
+      startDate: "02.2020",
+      endDate: "01.2022",
+      location: "Tallinn, Estonia",
+    },
+    {
+      school: "Best School",
+      degree: "bakalaurus",
+      startDate: "02.2023",
+      endDate: "01.2024",
+      location: "Tallinn, Estonia",
+    },
+  ],
+  experienceInfo: [
+    {
+      company: "Microsoft",
+      positionTitle: "Front-end Developer",
+      startJobDate: "02.2024",
+      endJobDate: "03.2025",
+      jobLocation: "New York, USA",
+      description:
+        "Developing new product features based on business requirements",
+    },
+    {
+      company: "Wolt",
+      positionTitle: "Front-end Developer",
+      startJobDate: "10.2021",
+      endJobDate: "12.2023",
+      jobLocation: "Tallinn, Estonia",
+      description:
+        "Developing new product features based on business requirements",
+    },
+  ],
+};
+
+export default exampleCVData;
